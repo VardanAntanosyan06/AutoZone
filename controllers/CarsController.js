2073f6df051a91a501f4ba0df07d34562f17c872
VardanAntanosyan06 <vantanosyan99@gmail.com>
2023-11-25 14:45:56 +0400

    aded referch dates

--- controllers/CarsController.js
+++ controllers/CarsController.js
@@ -308,6 +308,7 @@ const UpdateAllCardata = async (req, res) => {
   try {
     let { techNumber, phoneNumber } = req.body;
 
+    console.log(techNumber,phoneNumber);
     const User = await Users.findOne({ where: { phoneNumber } });
     const Car = await Cars.findOne({ where: { carTechNumber: techNumber } });
 
